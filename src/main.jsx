10610b8e17f4ac8498acb44da1b15ba365806d4e
karim <karim@example.com>
2024-11-24 11:40:32 +0530

    i makes changes in vite.config.js and main.js

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './stores/context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<BrowserRouter basename='/e-commerce/'>
   <React.StrictMode>
     <CartProvider>
     <App />
